1314962a561ee520f5d81f0fdbee9d3e205b419f
Iqbal0077 <143544219+Iqbal0077@users.noreply.github.com>
2023-08-30 00:29:06 +0600

    I have upload html css and js portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+/*================ typed js ================*/
+const typed = new Typed('.multiple-text', {
+strings: ['Frontend Developer', 'Backend Developer', 'Laravel Developer', 'Web Developer'],
+typeSpeed: 50,
+backSpeed: 50,
+backDelay: 2500,
+loop: true
+
+});
